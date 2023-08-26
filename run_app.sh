78cd842b7e725c50f214e14deb01628d9915d278
Aditya Anand <beadityaanand@gamail.com>
2023-08-26 23:15:51 +0530

    added repo name to image name

--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,8 @@
 # Building the docker image
-docker build -t fileupload:latest .
+docker build -t coutaditya/fileupload:latest .
 
 # Running the docker container
-docker run -p  5000:5000 fileupload:latest
+docker run -p  5000:5000 coutaditya/fileupload:latest
 
 # chmod +x run_container.sh => gives the necessary permissions (+x => add execute permission)
 # needed to run the script
